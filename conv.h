f5d79061feecb581134fa712488d6320571f0900
lirong_wu <37490744+LirongWu@users.noreply.github.com>
2019-08-30 16:25:30 +0800

    Revert "Upload"
    This reverts commit 578e247e4ee18232baa9f8f8311d0983bd68daf1.

---
+++ conv.h
@@ -0,0 +1,134 @@
+#ifndef CONV_H
+#define CONV_H
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include "math.h"
+#include "ap_int.h"
+
+#define img_num 16
+#define moments (int)4294967296 
+
+// Quantize forward output
+#define CONV_FORWARD 0 
+#define BATCH_FORWARD 0 
+#define SCALE_FORWARD 0 
+#define FULL_FORWARD 0 
+
+// Quantize backward indiff
+#define FULL_BACKWARD 0 
+#define SCALE_BACKWARD 0 
+#define BATCH_BACKWARD 0 
+#define CONV_BACKWARD 0 
+
+// Quantize weight and bias
+#define CONV_WEIGHT 0 
+#define SCALE_WEIGHT 0 
+#define SCALE_BIAS 0  
+#define FULL_WEIGHT 0 
+#define FULL_BIAS 0  
+
+// Show debug information
+#define TEST_MODE 0
+#define PRINT_LOSS 1
+#define PRINT_ACCURACY 1
+#define SHOW_PAIRS 0 
+
+// Show forward results
+#define SHOW_CONV_IN 0
+#define SHOW_BATCH_IN 0
+#define SHOW_SCALE_IN 0
+#define SHOW_RELU_IN 0
+#define SHOW_POOL_IN 0
+#define SHOW_FULL_IN 0
+#define SHOW_SOFTMAX_IN 0
+#define SHOW_SOFTMAX_OUT 0
+
+// Show backward results (diff)
+#define SHOW_CONV_INDIFF 0
+#define SHOW_BATCH_INDIFF 0
+#define SHOW_SCALE_INDIFF 0
+#define SHOW_RELU_INDIFF 0
+#define SHOW_POOL_INDIFF 0
+#define SHOW_FULL_INDIFF 0
+#define SHOW_SOFTMAX_INDIFF 0
+
+// Show weights and bias
+#define SHOW_CONV_WEIGHT 0
+#define SHOW_FULL1_WEIGHT 0
+#define SHOW_FULL1_BIAS 0
+#define SHOW_FULL2_WEIGHT 0
+#define SHOW_FULL2_BIAS 0
+#define SHOW_SCALE_WEIGHT 0
+#define SHOW_SCALE_BIAS 0
+
+int input_qt(float input, int s_in, int *larger, int *smaller, int *all, bool stochastic);
+
+void memset_float(float *to, int size);
+
+void quantize(float *to, int size, int *step, bool if_stochastic); 
+
+
+void training(float *data, float *wg, float *fp, int *setting, int *sp, int rate, int epoch, int decay);
+
+
+void conv(float *input, float *weights, float *tmp, float *output, int *params, int *sparams);
+
+void conv_back(float *outdiff, float *weights, float *indiff, float *tmp, int *params, int *sparams);
+
+void weight_back(float *outdiff, float *weights, float *input, float *moment, int *params, int *sparams, int rate);
+
+
+
+void fc(float *input, float *weights, float *bias,
+    float *output, int *params, int *sparams);
+
+void back_fc(float *outdiff, float *weights, float *tmp,
+    float *indiff, int *params, int *sparams);
+
+void fc_weight(float *outdiff, float *weights,  float *input, float *moment, int *params, int *sparams, int rate);
+
+void bias_back_fc(float *outdiff, float *bias, float *moment, int *params, int *sparams, int rate);
+
+
+void pool(float *input, float *pos,
+    float *output, int *params, int *sparams);
+
+void back_pool(float *indiff, float *pos,
+    float *outdiff0, float *outdiff1, int *params, int *sparams);
+
+
+void relu(float *input,
+    float *output, int *params, int *sparams);
+
+void relu_back(float *input,
+    float *outdiff0, float *outdiff1, float *indiff, int *params, int *sparams);
+
+
+void eltwise(float *input0, float *input1, float *output, int *params);
+
+void eltwise_back(float *outdiff, float *indiff, int *params);
+
+
+void batch(float *input, float *temp,
+  float *output, int *params, int *sparams);
+
+void batch_back(float *output, float *indiff, float *temp,
+    float *outdiff,  int *params, int *sparams);
+
+
+void scale(float *input, float *weights, float *bias,
+    float *output, int *params, int *sparams);
+
+void scale_back(float *indiff, float *weights, float *outdiff, int *params, int *sparams); 
+
+void weight_scale(float *outdiff, float *weights,  float *input, float *moment, int *params, int *sparams, int rate);
+
+void bias_scale(float *bias, float *outdiff, float *moment, int *params, int *sparams, int rate);
+
+
+void softmax(float *input, float *output, int *params, int *sparams, float *diff, float *tmp);
+
+#endif
---
+++ pooling.cpp
@@ -0,0 +1,170 @@
+#include "conv.h"
+
+#define PARA 10
+
+void pool(float *input, float *pos, float *output, int *params, int *sparams)
+{
+	float in_buf[64][PARA*img_num];
+
+    int inChannel = params[0];
+	int outChannel = params[1];
+	int inCh_once = params[2];
+	int outCh_once = params[3];
+	int iteration_out = outChannel/PARA;
+	int x_dim = params[4];
+	int y_dim = params[5];
+	int stride = params[6];
+	int pad = params[7];
+	int ksize = params[8];
+	int maxpool = params[9];
+
+	int x_out = ((x_dim - ksize) / stride) + 1;
+    int y_out = x_out;
+
+	for(int o = 0; o < iteration_out; o++)
+		for(int y = 0; y < y_out; y++)
+	    	for(int x = 0; x < x_out; x++)
+	    	{
+	            for(int p = 0; p<ksize; p++)
+	        		for(int q = 0; q<ksize; q++)
+	        		{
+	        			int x_in = x*stride+q;
+						int y_in = y*stride+p;
+						int inoff = p*ksize+q;
+			    		int insize = PARA*img_num;
+						if((stride + q < ksize) && x != 0)
+						{
+							memcpy(in_buf[inoff], in_buf[p*ksize+stride+q], insize*4);
+						}
+						else
+						{
+							int in_off = ((y_in*x_dim+x_in)*inChannel+o*PARA)*img_num;
+							memcpy(in_buf[inoff], input + in_off, insize*4);                                           
+						}
+					}
+                  
+				#pragma omp parallel for
+	          	for(int p = 0; p<PARA; p++)
+	    	    	for(int im = 0; im<img_num; im++)
+	    	    	{
+						float max = 0;
+						float idx = 0;
+						float sum = 0;
+	            		for(int m = 0; m<ksize; m++)
+	              			for(int n = 0; n<ksize; n++)
+	             			{
+	               				float ip = in_buf[m*ksize+n][p*img_num+im];
+	               				if(maxpool)
+	               				{
+									if(ip>max)
+	                 				{
+	            	   					idx = m*ksize+n;
+	            	   					max = ip;
+	                 				}
+	               				}
+	               				else 
+	            	   				sum += ip;
+	             			}
+
+	             		int out_off = ((y*x_out+x)*outChannel + o*PARA + p)*img_num+im;
+
+	             		if(maxpool)
+	             		{
+	             			output[out_off] = max;
+	             			pos[out_off] = idx;
+	             		}
+	             		else
+	             			output[out_off] = sum/ksize/ksize;
+
+	    	   		}
+	        }
+}
+
+
+void back_pool(float *indiff, float *pos, float *outdiff0, float *outdiff1, int *params, int *sparams)
+{
+	float out_buf[PARA][img_num];
+	float out0_buf[PARA][img_num];
+	float pos_buf[PARA][img_num];
+
+	int inChannel = params[0];
+	int outChannel = params[1];
+	int inCh_once = params[2];
+	int outCh_once = params[3];
+	int iteration_out = outChannel/PARA;
+	int x_dim = params[4];
+	int y_dim = params[5];
+	int stride = params[6];
+	int dup = params[7];
+	int ksize = params[8];
+	int maxpool = params[9];
+
+	int x_out = ((x_dim - ksize) / stride) + 1;
+    int y_out = x_out;
+
+ 	for(int o = 0; o<iteration_out; o++)
+		for(int y = 0; y < y_out; y++)
+  			for(int x = 0; x < x_out; x++)
+	 		{
+				int out_trans_size = PARA;
+				int out_offset = ((y*x_out+x)*outChannel+o*PARA);
+				memcpy(out_buf, outdiff0 + out_offset*img_num, out_trans_size*img_num*4);
+				if(dup)
+				{
+				memcpy(out0_buf, outdiff1 + out_offset*img_num, out_trans_size*img_num*4);
+				}
+
+				memcpy(pos_buf, pos + out_offset*img_num, out_trans_size*img_num*4);
+
+        		#pragma omp parallel for
+				for(int p = 0; p<PARA; p++)
+		 		for(int im = 0 ;im<img_num ;im++)	
+				{
+					float tmpout;
+					if(dup)
+					{
+						tmpout = out_buf[p][im] + out0_buf[p][im];          
+					}
+					else
+						tmpout = out_buf[p][im];
+
+					if(maxpool)
+					{ 	
+						float tmp[49];
+						memset_float(tmp, 49);
+						int idx = int(pos_buf[p][im]);
+						tmp[idx] += tmpout;
+
+        				for(int m = 0; m<ksize; m++)
+           					for(int n = 0; n<ksize; n++)
+            				{
+								int x_in = x*stride+n;
+								int y_in = y*stride+m;
+								int in_off = ((y_in*x_dim+x_in)*inChannel+o*PARA+p)*img_num+im;
+								float tmp_int;
+								if (((stride + n) < ksize && x!= 0) || ((stride + m) < ksize && y!=0))
+									tmp_int = indiff[in_off] + tmp[m*ksize+n];
+								else
+									tmp_int = tmp[m*ksize+n];
+									
+								indiff[in_off] = tmp_int;
+							}
+					}
+					else
+						for(int m = 0; m<ksize; m++)
+							for(int n = 0; n<ksize; n++)
+							{
+								int x_in = x*stride + n;
+								int y_in = y*stride + m;
+								int in_off = ((y_in*x_dim+x_in)*inChannel+o*PARA+p)*img_num+im;
+								float tmp_int;
+								if (((stride + n) < ksize && x!= 0) || ((stride + m) < ksize && y!=0))
+									tmp_int = indiff[in_off] + tmpout/ksize/ksize;
+								else
+									tmp_int = tmpout /ksize/ksize;  
+									
+								indiff[in_off] = tmp_int;
+							}
+				}
+	 		}
+}
